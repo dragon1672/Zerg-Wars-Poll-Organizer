21a09e5d20e705707306f12cd0e270ca19951b5f
Anthony Corbin <dragon1672@gmail.com>
2025-10-10 15:50:23 -0400

    Improving loading strat and fix issue with package loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Zerg-Wars-Poll-Organizer/',
       server: {
         port: 3000,
         host: '0.0.0.0',
